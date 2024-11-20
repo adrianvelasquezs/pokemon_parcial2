6906fc4551d6107cb133758966b9c393aa246392
Adrian Velasquez <a.velasquezs@uniandes.edu.co>
2024-11-20 09:33:25 -0500

    modificacion routing

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonListarComponent } from './pokemon-listar/pokemon-listar.component';
 import { PokemonDetalleComponent } from './pokemon-detalle/pokemon-detalle.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
     PokemonListarComponent,
